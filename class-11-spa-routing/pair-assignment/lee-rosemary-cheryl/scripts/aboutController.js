79dfcf860eeb9e9a91730b4b97ece50d280a0176
Lee Zamastil <l.zamastil@gmail.com>
2016-09-28 20:25:41 -0700

    Solved TODOs via Rosemary, Cheryl and Lee

--- class-11-spa-routing/pair-assignment/lee-rosemary-cheryl/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/lee-rosemary-cheryl/scripts/aboutController.js
@@ -1,10 +1,13 @@
 (function(module) {
-  var aboutController = {};
+    var aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-  aboutController.index = function() {
+    // TODO: -DONE- Define a function that hides all main section elements, and then reveals just the #about section:
+    aboutController.index = function() {
 
-  };
+            $('#articles').hide();
+            $('#about').show();
 
-  module.aboutController = aboutController;
+    };
+
+    module.aboutController = aboutController;
 })(window);
--- class-11-spa-routing/pair-assignment/lee-rosemary-cheryl/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/lee-rosemary-cheryl/scripts/routes.js
@@ -1,4 +1,12 @@
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
+// TODO: -DONE- Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute!
+page('/', articlesController.index);
+page('/about', aboutController.index);
+
+
+
+
+// TODO: -DONE- What function do you call to activate page.js? Fire it off now, to execute!
+
+page();
